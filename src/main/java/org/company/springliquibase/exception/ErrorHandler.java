6fed0f826dc73e0404dcf7c6038e8847701b226d
Togrul <totu7771@gmail.com>
2025-02-21 17:01:11 +0400

    Exception handling update

--- src/main/java/org/company/springliquibase/exception/ErrorHandler.java
+++ src/main/java/org/company/springliquibase/exception/ErrorHandler.java
@@ -1,15 +1,14 @@
 package org.company.springliquibase.exception;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import static org.company.springliquibase.constants.ExceptionConstants.*;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.CONFLICT;
 
 @Slf4j
 @RestControllerAdvice
@@ -17,29 +16,26 @@ public class ErrorHandler {
     private static final String HANDLE_EXCEPTION_ERROR = "ActionLog.handleException.error";
 
     @ExceptionHandler(Exception.class)
-    @ResponseStatus(INTERNAL_SERVER_ERROR)
-    public ErrorResponse handleException(Exception e) {
+    public ResponseEntity<ErrorResponse> handleException(Exception e) {
         log.error(HANDLE_EXCEPTION_ERROR, e);
-        return new ErrorResponse(UNEXPECTED_EXCEPTION.getMessage());
+        return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ErrorResponse(UNEXPECTED_EXCEPTION.getMessage()));
     }
 
     @ExceptionHandler(UserNotFoundException.class)
-    @ResponseStatus(NOT_FOUND)
-    public ErrorResponse handleException(UserNotFoundException e) {
+    public ResponseEntity<ErrorResponse> handleException(UserNotFoundException e) {
         log.error(HANDLE_EXCEPTION_ERROR, e);
-        return new ErrorResponse(USER_NOT_FOUND.getMessage());
+        return ResponseEntity.status(NOT_FOUND).body(new ErrorResponse(USER_NOT_FOUND.getMessage()));
     }
 
     @ExceptionHandler(CardNotFoundException.class)
-    @ResponseStatus(NOT_FOUND)
-    public ErrorResponse handleException(CardNotFoundException e) {
+    public ResponseEntity<ErrorResponse> handleException(CardNotFoundException e) {
         log.error(HANDLE_EXCEPTION_ERROR, e);
-        return new ErrorResponse(CARD_NOT_FOUND.getMessage());
+        return ResponseEntity.status(NOT_FOUND).body(new ErrorResponse(CARD_NOT_FOUND.getMessage()));
     }
 
     @ExceptionHandler(UserAlreadyExistsException.class)
-    public ResponseEntity<String> handleUserAlreadyExistsException(UserAlreadyExistsException ex) {
+    public ResponseEntity<ErrorResponse> handleUserAlreadyExistsException(UserAlreadyExistsException ex) {
         log.error("Error occurred: {}", ex.getMessage());
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);
+        return ResponseEntity.status(CONFLICT).body(new ErrorResponse(ex.getMessage()));
     }
 }
--- src/main/java/org/company/springliquibase/exception/ErrorResponse.java
+++ src/main/java/org/company/springliquibase/exception/ErrorResponse.java
@@ -1,13 +1,8 @@
 package org.company.springliquibase.exception;
 
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.experimental.FieldDefaults;
 
-@Getter
-@AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class ErrorResponse {
-    String message;
+public record ErrorResponse(String message) {
 }
